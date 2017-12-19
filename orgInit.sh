fa1a5faa2f5c2ff5121e4f3569d3f36dc18d3525
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-19 17:39:47 -0600

    password:set

--- orgInit.sh
+++ orgInit.sh
@@ -3,5 +3,6 @@ sfdx force:org:create -f config/project-scratch-def.json -s -a ducatiCustomerExp
 sfdx force:source:push
 sfdx force:user:permset:assign -n Ducati_App
 sfdx force:data:tree:import -p data/importPlan.json
-sfdx force:user:password:generate
+sfdx msm:user:password:set -l User -g User -p sfdx1234 --json
+
 sfdx force:org:open
